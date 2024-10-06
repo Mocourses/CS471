b4630ca2b6d3255c3b7989ea218febb9153b8207
Mocourses <mo4courses@gmail.com>
2024-10-06 13:11:06 +0300

    Lab4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
  path('', views.index),
  path('index2/<int:val1>/', views.index2),
- path('<int:bookId>', views.viewbook)
+ path('<int:bookId>', views.viewbook),
+
+ #Lab_4
+ path('', views.index, name= "books.index"),
+ path('list_books/', views.list_books, name= "books.list_books"),
+ path('<int:bookId>/', views.viewbook, name="books.one_book"),
+ path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
